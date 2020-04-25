2bd30f680446cc2a7d81e8e758d0b4cfe4fc76d2
Nness <Nness@users.noreply.github.com>
2020-04-26 11:44:17 +1200

    add is set optional collection test.

--- tests/Optional.Tests/OptionalCollectionTests.cs
+++ tests/Optional.Tests/OptionalCollectionTests.cs
@@ -20,10 +20,35 @@ namespace Nness.Text.Json.Tests
             get {
                 var data = new TheoryData<OptionalCollection<int>, bool, ICollection<int>?>
                 {
+                    {default, false, default},
                     {new OptionalCollection<int>(OptionalState.Null), false, default},
                     {new OptionalCollection<int>(OptionalState.Undefined), false, default},
                     {new OptionalCollection<int>(Array.Empty<int>()), true, Array.Empty<int>()},
                     {new OptionalCollection<int>(new[] {1}), true, new[] {1}}
+                };
+
+                return data;
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(IsSetSamples))]
+        public void IsSet(OptionalCollection<int> list, bool expectIsSet)
+        {
+            bool actualIsSet = list.IsSet();
+
+            Assert.Equal(expectIsSet, actualIsSet);
+        }
+
+        public static TheoryData<OptionalCollection<int>, bool> IsSetSamples {
+            get {
+                var data = new TheoryData<OptionalCollection<int>, bool>
+                {
+                    { default, false },
+                    { new OptionalCollection<int>(OptionalState.Null), true },
+                    { new OptionalCollection<int>(OptionalState.Undefined), false},
+                    { new OptionalCollection<int>(Array.Empty<int>()), true },
+                    { new OptionalCollection<int>(new[] {1}), true },
                 };
 
                 return data;
