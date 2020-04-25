ee2c4b28973dc9876c17eea6262ba66b99eb6de0
Nness <Nness@users.noreply.github.com>
2020-04-26 11:36:46 +1200

    add has value optional collection test.

---
+++ tests/Optional.Tests/OptionalCollectionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Nness.Text.Json.Tests
+{
+    public class OptionalCollectionTests
+    {
+        [Theory]
+        [MemberData(nameof(HasValueSamples))]
+        public void HasValue(OptionalCollection<int> list, bool expectHasValue, ICollection<int>? expectValue)
+        {
+            bool actualHasValue = list.HasValue(out ICollection<int>? actualValue);
+
+            Assert.Equal(expectHasValue, actualHasValue);
+            Assert.Equal(expectValue, actualValue);
+        }
+
+        public static TheoryData<OptionalCollection<int>, bool, ICollection<int>?> HasValueSamples {
+            get {
+                var data = new TheoryData<OptionalCollection<int>, bool, ICollection<int>?>
+                {
+                    {new OptionalCollection<int>(OptionalState.Null), false, default},
+                    {new OptionalCollection<int>(OptionalState.Undefined), false, default},
+                    {new OptionalCollection<int>(Array.Empty<int>()), true, Array.Empty<int>()},
+                    {new OptionalCollection<int>(new[] {1}), true, new[] {1}}
+                };
+
+                return data;
+            }
+        }
+    }
+}
