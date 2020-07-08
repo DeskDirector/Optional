ea8f41d98a6290d4382afbce56e0cbd40d72257c
Nness <Nness@users.noreply.github.com>
2020-07-08 21:00:42 +1200

    implement not empty validator.

---
+++ src/Optional.FluentValidation/ValidatorExtensions.NotEmpty.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using FluentValidation;
+using Nness.Text.Json.Validation.Validators;
+
+namespace Nness.Text.Json.Validation
+{
+    public static partial class ValidatorExtensions
+    {
+        /// <summary>
+        /// Defines a 'not empty' validator on the current rule builder. Validation will fail if the
+        /// property is empty string or null. It is used for required field. Either undefined or required.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<string>> NotEmpty<T>(
+            this IRuleBuilder<T, Optional<string>> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new NotEmptyValidator<string>());
+        }
+
+        /// <summary>
+        /// Defines a 'not empty' validator on the current rule builder. Validation will fail if the
+        /// property empty collection or null. It is used for required field. Either undefined or required.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <typeparam name="TProperty">Type of property being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<TProperty>> NotEmpty<T, TProperty>(
+            this IRuleBuilder<T, Optional<TProperty>> ruleBuilder)
+            where TProperty : IEnumerable
+        {
+            return ruleBuilder.SetValidator(new NotEmptyValidator<TProperty>());
+        }
+
+        /// <summary>
+        /// Defines a 'not empty' validator on the current rule builder. Validation will fail if the
+        /// property empty collection or null. It is used for required field. Either undefined or required.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <typeparam name="TProperty">Type of property being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, OptionalCollection<TProperty>> NotEmpty<T, TProperty>(
+            this IRuleBuilder<T, OptionalCollection<TProperty>> ruleBuilder)
+            where TProperty : IEnumerable
+        {
+            return ruleBuilder.SetValidator(new NotEmptyValidator<TProperty>());
+        }
+    }
+}
---
+++ src/Optional.FluentValidation/Validators/NotEmptyValidator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Resources;
+using FluentValidation.Validators;
+
+namespace Nness.Text.Json.Validation.Validators
+{
+    public class NotEmptyValidator<T> : PropertyValidator
+    {
+        public NotEmptyValidator()
+            : base(new LanguageStringSource(nameof(NotEmptyValidator)))
+        {
+            Options.ErrorCodeSource = ValidatorHelper.ErrorCodeSource(typeof(NotEmptyValidator<>));
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            if (!(context.PropertyValue is IOptional optional)) {
+                return true;
+            }
+
+            if (optional.IsUndefined()) {
+                return true;
+            }
+
+            if (optional.IsNull()) {
+                return false;
+            }
+
+            switch (optional) {
+                case Optional<T> normal:
+                    return IsValid(normal);
+
+                case OptionalCollection<T> collection:
+                    return IsValid(collection);
+
+                default:
+                    throw new InvalidOperationException($"Type {optional.GetType().FullName} is not expected");
+            }
+        }
+
+        private static bool IsValid(OptionalCollection<T> optional)
+        {
+            ICollection<T>? value = optional.Value;
+            return value != null && value.Count > 0;
+        }
+
+        private static bool IsValid(Optional<T> optional)
+        {
+            T value = optional.Value;
+            if (value == null) {
+                return false;
+            }
+
+            if (typeof(T) == typeof(string)) {
+                return IsValidString(value);
+            }
+
+            return !typeof(IEnumerable).IsAssignableFrom(typeof(T)) || IsValidCollection(value);
+        }
+
+        private static bool IsValidString(T value)
+        {
+            if (!(value is string str)) {
+                throw new InvalidOperationException($"Value should be string, but it is not Type: {typeof(T).FullName}");
+            }
+
+            return !String.IsNullOrWhiteSpace(str);
+        }
+
+        private static bool IsValidCollection(T value)
+        {
+            if (value == null) {
+                return false;
+            }
+
+            if (!(value is IEnumerable enumerable)) {
+                throw new InvalidOperationException($"Value should be IEnumerable, but it is not. Type: {typeof(T).FullName}");
+            }
+
+            return enumerable.Cast<object>().Any();
+        }
+    }
+}
