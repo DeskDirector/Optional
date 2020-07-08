9179ea2f9b9b6f94951676cb8c0f2000de43ead2
Nness <Nness@users.noreply.github.com>
2020-07-08 21:07:21 +1200

    implement notnull validator.

---
+++ src/Optional.FluentValidation/ValidatorExtensions.NotNull.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Nness.Text.Json.Validation.Validators;
+
+namespace Nness.Text.Json.Validation
+{
+    public static partial class ValidatorExtensions
+    {
+        /// <summary>
+        /// Defines a 'not null' validator on the current rule builder. Validation will fail if the
+        /// property is null. It is used for required field. Either undefined or required.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, IOptional> NotNull<T>(
+            this IRuleBuilder<T, IOptional> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new NotNullValidator());
+        }
+    }
+}
---
+++ src/Optional.FluentValidation/Validators/NotNullValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Resources;
+using FluentValidation.Validators;
+
+namespace Nness.Text.Json.Validation.Validators
+{
+    public class NotNullValidator : PropertyValidator
+    {
+        public NotNullValidator()
+            : base(new LanguageStringSource(nameof(NotNullValidator)))
+        {
+            Options.ErrorCodeSource = ValidatorHelper.ErrorCodeSource(typeof(NotNullValidator));
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            if (!(context.PropertyValue is IOptional optional)) {
+                return true;
+            }
+
+            if (optional.IsUndefined()) {
+                return true;
+            }
+
+            return !optional.IsNull();
+        }
+    }
+}
