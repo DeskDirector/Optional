7517ee8af0736c538992c36ca312bc634c95e18f
Nness <Nness@users.noreply.github.com>
2020-07-08 20:50:19 +1200

    implement string length validator.

---
+++ src/Optional.FluentValidation/ValidatorExtensions.Length.cs
@@ -0,0 +1,71 @@
+﻿using FluentValidation;
+using Nness.Text.Json.Validation.Validators;
+
+namespace Nness.Text.Json.Validation
+{
+    public static partial class ValidatorExtensions
+    {
+        /// <summary>
+        /// Defines a length validator on the current rule builder, but only for string properties.
+        /// Validation will fail if the length of the string is outside of the specified range. The
+        /// range is inclusive.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<string>> Length<T>(
+            this IRuleBuilder<T, Optional<string>> ruleBuilder,
+            int min,
+            int max)
+        {
+            return ruleBuilder.SetValidator(new LengthValidator(min, max));
+        }
+
+        /// <summary>
+        /// Defines a length validator on the current rule builder, but only for string properties.
+        /// Validation will fail if the length of the string is not equal to the length specified.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="exactLength"></param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<string>> Length<T>(
+            this IRuleBuilder<T, Optional<string>> ruleBuilder,
+            int exactLength)
+        {
+            return ruleBuilder.SetValidator(new ExactLengthValidator(exactLength));
+        }
+
+        /// <summary>
+        /// Defines a length validator on the current rule builder, but only for string properties.
+        /// Validation will fail if the length of the string is larger than the length specified.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="maximumLength"></param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<string>> MaximumLength<T>(
+            this IRuleBuilder<T, Optional<string>> ruleBuilder,
+            int maximumLength)
+        {
+            return ruleBuilder.SetValidator(new MaximumLengthValidator(maximumLength));
+        }
+
+        /// <summary>
+        /// Defines a length validator on the current rule builder, but only for string properties.
+        /// Validation will fail if the length of the string is less than the length specified.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="minimumLength"></param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<string>> MinimumLength<T>(
+            this IRuleBuilder<T, Optional<string>> ruleBuilder,
+            int minimumLength)
+        {
+            return ruleBuilder.SetValidator(new MinimumLengthValidator(minimumLength));
+        }
+    }
+}
---
+++ src/Optional.FluentValidation/Validators/LengthValidator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using FluentValidation.Resources;
+using FluentValidation.Validators;
+
+namespace Nness.Text.Json.Validation.Validators
+{
+    public class LengthValidator : PropertyValidator, ILengthValidator
+    {
+        public int Min { get; }
+
+        public int Max { get; }
+
+        public LengthValidator(int min, int max)
+            : this(
+                min: min,
+                max: max,
+                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(CountValidator<>)),
+                errorSource: new LanguageStringSource(nameof(LengthValidator))
+                )
+        { }
+
+        internal LengthValidator(int min, int max, IStringSource errorCodeSource, IStringSource errorSource)
+            : base(errorSource)
+        {
+            Max = max;
+            Min = min;
+
+            if (max != -1 && max < min) {
+                throw new ArgumentOutOfRangeException(nameof(max), "Max should be larger than min.");
+            }
+
+            Options.ErrorCodeSource = errorCodeSource;
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            if (!(context.PropertyValue is Optional<string> optional)) {
+                return true;
+            }
+
+            if (!optional.HasValue(out string? value)) {
+                return true;
+            }
+
+            int min = Min;
+            int max = Max;
+
+            int length = value.Length;
+
+            if (length >= min && (max == -1 || length <= max)) {
+                return true;
+            }
+
+            context.MessageFormatter
+                .AppendArgument("MinLength", min)
+                .AppendArgument("MaxLength", max)
+                .AppendArgument("TotalLength", length);
+
+            return false;
+        }
+    }
+
+    public class ExactLengthValidator : LengthValidator
+    {
+        public ExactLengthValidator(int length)
+            : base(
+                min: length,
+                max: length,
+                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(ExactLengthValidator)),
+                errorSource: new LanguageStringSource(nameof(ExactLengthValidator))
+                )
+        { }
+    }
+
+    public class MaximumLengthValidator : LengthValidator
+    {
+        public MaximumLengthValidator(int max)
+            : base(
+                min: 0,
+                max: max,
+                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(MaximumLengthValidator)),
+                errorSource: new LanguageStringSource(nameof(MaximumLengthValidator))
+                )
+        { }
+    }
+
+    public class MinimumLengthValidator : LengthValidator
+    {
+        public MinimumLengthValidator(int min)
+            : base(
+                min: min,
+                max: -1,
+                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(MinimumLengthValidator)),
+                errorSource: new LanguageStringSource(nameof(MinimumLengthValidator))
+                )
+        { }
+    }
+}
