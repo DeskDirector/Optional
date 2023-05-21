90c3ee4a8122240b0516abe847ed97c2d0be001c
Nness <Nness@users.noreply.github.com>
2023-05-21 21:36:17 +1200

    implement count validator #3

--- src/Optional.FluentValidation/Validators/CountValidator.cs
+++ src/Optional.FluentValidation/Validators/CountValidator.cs
@@ -1,32 +1,20 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using FluentValidation.Resources;
+using FluentValidation;
 using FluentValidation.Validators;
 
 namespace Nness.Text.Json.Validation.Validators
 {
-    public class CountValidator<T> : PropertyValidator, ILengthValidator
+    public class CountValidator<TModel, TCollection, T> : PropertyValidator<TModel, IOptional<TCollection>>, ILengthValidator
+        where TCollection : ICollection<T>
     {
-        private const string ErrorTemplate =
-            "'{PropertyName}' collection must be between {MinCount} and {MaxCount} items. Collection has {TotalCount} items";
-
         public int Min { get; }
 
         public int Max { get; }
 
-        public CountValidator(int min, int max)
-            : this(
-                min: min,
-                max: max,
-                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(CountValidator<>)),
-                errorSource: new StaticStringSource(ErrorTemplate)
-                )
-        {
-        }
+        public override string Name => "OptionalListCountValidator";
 
-        internal CountValidator(int min, int max, IStringSource errorCodeSource, IStringSource errorSource)
-            : base(errorSource)
+        internal CountValidator(int min, int max)
         {
             Max = max;
             Min = min;
@@ -35,99 +23,64 @@ namespace Nness.Text.Json.Validation.Validators
                 throw new ArgumentOutOfRangeException(nameof(min), "Min for list count should be bigger or equal to zero");
             }
 
-            if (max != -1 && max < min) {
+            if (max != -1 && max <= min) {
                 throw new ArgumentOutOfRangeException(nameof(max), "Max should be larger than min.");
             }
-
-            Options.ErrorCodeSource = errorCodeSource;
         }
 
-        protected override bool IsValid(PropertyValidatorContext context)
+        public override bool IsValid(ValidationContext<TModel> context, IOptional<TCollection>? optional)
         {
-            if (context.PropertyValue is not IOptional optional) {
+            if (optional == null) {
                 return true;
             }
 
-            if (optional.IsNull() || optional.IsUndefined()) {
-                return true;
-            }
-
-            if (optional is OptionalCollection<T> collection) {
-                return IsValid(collection, context);
-            }
-
-            return IsValid(optional, context);
-        }
-
-        private bool IsValid(OptionalCollection<T> optional, PropertyValidatorContext context)
-        {
-            if (!optional.HasValue(out ICollection<T>? value)) {
+            if (!optional.HasValue(out TCollection? value)) {
                 return true;
             }
 
             int count = value.Count;
-            return IsValid(count, context);
-        }
-
-        private bool IsValid(IOptional optional, PropertyValidatorContext context)
-        {
-            if (!optional.HasValue(out object? value)) {
+            if (count >= Min && (Max == -1 || count <= Max)) {
                 return true;
             }
 
-            if (value is not ICollection collection) {
-                return true;
+            if (Min > 0) {
+                _ = context.MessageFormatter.AppendArgument("MinElements", Min);
             }
 
-            int count = collection.Count;
-            return IsValid(count, context);
-        }
-
-        private bool IsValid(int count, PropertyValidatorContext context)
-        {
-            int min = Min;
-            int max = Max;
-
-            if (count >= min && (max == -1 || count <= max)) {
-                return true;
+            if (Max > 0) {
+                _ = context.MessageFormatter.AppendArgument("MaxElements", Max);
             }
-
-            context.MessageFormatter
-                .AppendArgument("MinCount", min)
-                .AppendArgument("MaxCount", max)
-                .AppendArgument("TotalCount", count);
 
             return false;
         }
+
+        protected override string GetDefaultMessageTemplate(string errorCode)
+        {
+            if (Min <= 0) {
+                return "{PropertyName} must not contain more than {MaxElements} items.";
+            }
+
+            if (Max <= 0) {
+                return "{PropertyName} must container at least {MinElements} items.";
+            }
+
+            return "{PropertyName} must not contain more than {MaxElements} items and at least {MinElements} items";
+        }
     }
 
-    public class MaximumCountValidator<T> : CountValidator<T>
+    public class MaximumCountValidator<TModel, TCollection, T> : CountValidator<TModel, TCollection, T>
+        where TCollection : ICollection<T>
     {
-        private const string ErrorTemplate =
-            "The count of '{PropertyName}' must be {MaxCount} items or fewer. Collection has {TotalCount} items";
-
         public MaximumCountValidator(int max)
-            : base(
-                min: 0,
-                max: max,
-                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(MaximumCountValidator<>)),
-                errorSource: new StaticStringSource(ErrorTemplate)
-            )
+            : base(min: 0, max: max)
         { }
     }
 
-    public class MinimumCountValidator<T> : CountValidator<T>
+    public class MinimumCountValidator<TModel, TCollection, T> : CountValidator<TModel, TCollection, T>
+        where TCollection : ICollection<T>
     {
-        private const string ErrorTemplate =
-            "The count of '{PropertyName}' must be at least {MaxCount} items. Collection has {TotalCount} items";
-
         public MinimumCountValidator(int min)
-            : base(
-                min: min,
-                max: -1,
-                errorCodeSource: ValidatorHelper.ErrorCodeSource(typeof(MinimumCountValidator<>)),
-                errorSource: new StaticStringSource(ErrorTemplate)
-            )
+            : base(min: min, max: -1)
         { }
     }
 }
