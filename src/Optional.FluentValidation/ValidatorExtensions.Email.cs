1385a058e08d7bfd61e7d95e622ca5ecc3d3c35a
Nness <Nness@users.noreply.github.com>
2023-05-21 22:16:05 +1200

    fix email validator #3

--- src/Optional.FluentValidation/ValidatorExtensions.Email.cs
+++ src/Optional.FluentValidation/ValidatorExtensions.Email.cs
@@ -9,27 +9,12 @@ namespace Nness.Text.Json.Validation
         /// <summary>
         /// Email validator without using Regex
         /// </summary>
-        public static IRuleBuilderOptions<T, string> Email<T>(
-            this IRuleBuilder<T, string> ruleBuilder)
+        public static IRuleBuilderOptions<T, IOptional<string>> Email<T>(
+            this IRuleBuilder<T, IOptional<string>> ruleBuilder)
         {
-            if (ruleBuilder == null) {
-                throw new ArgumentNullException(nameof(ruleBuilder));
-            }
+            ArgumentNullException.ThrowIfNull(ruleBuilder);
 
-            return ruleBuilder.SetValidator(new EmailValidator());
-        }
-
-        /// <summary>
-        /// Email validator without using Regex
-        /// </summary>
-        public static IRuleBuilderOptions<T, Optional<string>> Email<T>(
-            this IRuleBuilder<T, Optional<string>> ruleBuilder)
-        {
-            if (ruleBuilder == null) {
-                throw new ArgumentNullException(nameof(ruleBuilder));
-            }
-
-            return ruleBuilder.SetValidator(new EmailValidator());
+            return ruleBuilder.SetValidator(new EmailValidator<T>());
         }
     }
 }
