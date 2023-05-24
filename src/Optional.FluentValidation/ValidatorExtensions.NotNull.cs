e3c405b6fb8a25f1efc96dadba84f450bc33f817
Nness <Nness@users.noreply.github.com>
2023-05-24 21:10:11 +1200

    adjust not null validator #3

--- src/Optional.FluentValidation/ValidatorExtensions.NotNull.cs
+++ src/Optional.FluentValidation/ValidatorExtensions.NotNull.cs
@@ -10,12 +10,14 @@ namespace Nness.Text.Json.Validation
         /// property is null. It is used for required field. Either undefined or required.
         /// </summary>
         /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <typeparam name="TProperty">Type of T's property</typeparam>
         /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
         /// <returns></returns>
-        public static IRuleBuilderOptions<T, IOptional> NotNull<T>(
-            this IRuleBuilder<T, IOptional> ruleBuilder)
+        public static IRuleBuilderOptions<T, TProperty> NotNull<T, TProperty>(
+            this IRuleBuilder<T, TProperty> ruleBuilder)
+            where TProperty : IOptional
         {
-            return ruleBuilder.SetValidator(new NotNullValidator());
+            return ruleBuilder.SetValidator(new OptionalNotNullValidator<T, TProperty>());
         }
     }
 }
---
+++ src/Optional.FluentValidation/Validators/OptionalNotNullValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+namespace Nness.Text.Json.Validation.Validators
+{
+    public class OptionalNotNullValidator<TModel, TProperty> : PropertyValidator<TModel, TProperty>
+      where TProperty : IOptional
+    {
+        public override string Name => "OptionalNotNullValidator";
+
+        protected override string GetDefaultMessageTemplate(string errorCode) => "{PropertyName} cannot be null";
+
+        public override bool IsValid(ValidationContext<TModel> context, TProperty? optional)
+        {
+            if (optional == null) {
+                return true;
+            }
+
+            return !optional.IsNull();
+        }
+    }
+}
