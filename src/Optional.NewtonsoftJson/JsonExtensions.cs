06ff770bd93fe5292ee3948aedfa7ec0f3768828
Silverlancer <Nness@users.noreply.github.com>
2023-07-24 10:26:25 +1200

    initialize newtonsoft json project #3

---
+++ src/Optional.NewtonsoftJson/JsonExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nness.Text.Json;
+
+namespace Optional.NewtonsoftJson
+{
+    public static class JsonExtensions
+    {
+        public static bool CanSerialize(
+            this IOptional value,
+            JsonSerializer serializer,
+            [NotNullWhen(true)] out JToken? token)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            token = default;
+
+            if (value.HasValue(out object? model)) {
+                token = JToken.FromObject(model);
+                return true;
+            }
+
+            if (value.IsNull()) {
+                token = new JValue((object?)null);
+                return true;
+            }
+
+            if (value.IsUndefined()) {
+                return false;
+            }
+
+            throw new InvalidOperationException(
+                $"Optional with state {value.State} is either not supported or it doesn't contain value."
+            );
+        }
+    }
+}
