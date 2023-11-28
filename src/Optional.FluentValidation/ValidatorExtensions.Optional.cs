b47f6579ac2f568bde4ae0b82c647ef94342b308
Nness <Nness@users.noreply.github.com>
2023-11-28 21:46:33 +1300

    add optional validator

---
+++ src/Optional.FluentValidation/ValidatorExtensions.Optional.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentValidation;
+using Nness.Text.Json.Validation.Validators;
+
+namespace Nness.Text.Json.Validation
+{
+    public static partial class ValidatorExtensions
+    {
+        /// <summary>
+        /// Associates an instance of IValidator with the current property rule, IValidator will only
+        /// be used when Optional{T} has value.
+        /// </summary>
+        /// <typeparam name="T">Type of object being validated</typeparam>
+        /// <typeparam name="TProperty">Type of property being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="validator">validator</param>
+        /// <param name="ruleSets">rule sets</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, Optional<TProperty>> SetValidator<T, TProperty>(
+            this IRuleBuilder<T, Optional<TProperty>> ruleBuilder,
+            IValidator<TProperty> validator,
+            params string[] ruleSets)
+        {
+            ArgumentNullException.ThrowIfNull(ruleBuilder);
+            ArgumentNullException.ThrowIfNull(validator);
+
+            OptionalValidator<TProperty> optional = new(validator);
+            return ruleBuilder.SetValidator(optional, ruleSets);
+        }
+
+        public static void WhenHasValue<T, TProperty>(
+            this AbstractValidator<T> validator,
+            Func<T, Optional<TProperty>> select,
+            Action<IRuleBuilder<T, TProperty?>> validate)
+        {
+            ArgumentNullException.ThrowIfNull(validate);
+            ArgumentNullException.ThrowIfNull(select);
+            ArgumentNullException.ThrowIfNull(validate);
+
+            validator.When(t => select(t).HasValue(out TProperty? _), () => {
+                validate(validator.Transform(r => select(r), s => s.HasValue(out TProperty? value) ? value : default));
+            });
+        }
+    }
+}
---
+++ src/Optional.FluentValidation/Validators/OptionalValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Nness.Text.Json.Validation.Validators
+{
+    public class OptionalValidator<T> : AbstractValidator<Optional<T>>
+    {
+        private readonly IValidator<T> _validator;
+
+        public OptionalValidator(IValidator<T> validator)
+        {
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        protected override bool PreValidate(ValidationContext<Optional<T>> context, ValidationResult result)
+        {
+            Optional<T> instance = context.InstanceToValidate;
+            if (instance.IsUndefined()) {
+                return false;
+            }
+
+            if (!instance.HasValue(out T? value)) {
+                return true;
+            }
+
+            ValidationContext<T> internalContext = new(value, context.PropertyChain, context.Selector);
+            ValidationResult internalResult = _validator.Validate(internalContext);
+            foreach (ValidationFailure failure in internalResult.Errors) {
+                result.Errors.Add(failure);
+            }
+
+            return true;
+        }
+    }
+}
